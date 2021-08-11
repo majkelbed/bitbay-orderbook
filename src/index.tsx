5b78125d5ad396d6d3905dd160c8808d65c5943e
Michal Bednarczyk <bednarczyk.business@gmail.com>
2021-08-11 21:42:25 +0200

    app import fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App/components/App/App.component';
+import { App } from './App/App.component';
 
 ReactDOM.render(
   <React.StrictMode>
